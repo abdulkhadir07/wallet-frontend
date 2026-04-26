460e4ed3e39bd1a322b01409f1811a0e44badc1c
Abdul Khadir <abdulkhadirjallow743@gmail.com>
2026-04-25 23:59:07 -0700

    feat(auh): add token management utilities

---
+++ js/auth.js
@@ -0,0 +1,24 @@
+const TOKEN_KEY = "enum_token";
+
+function savedToken(token) {
+    localStorage.setItem(TOKEN_KEY, token)
+}
+
+function getToken() {
+    return localStorage.getItem(TOKEN_KEY);
+}
+
+function isLoggedIn() {
+    return getToken() !== null
+}
+
+function logout() {
+    localStorage.removeItem(TOKEN_KEY);
+    window.location.href = "../pages/login.html";
+}
+
+function requireAuth() {
+    if (!isLoggedIn()) {
+        window.location.href = "../pages/login.html"
+    };
+}
